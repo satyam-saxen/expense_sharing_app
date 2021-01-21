9eb39edd1249a2838dd537da4f36b2b3c7187d5f
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-22 00:51:35 +0530

    Fix bug for labels htmlFor attribute

--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -14,7 +14,7 @@ const SignUp = (props)=>{
 const Name = (props)=>{
     return(
         <div className="form-group">
-            <label for="inputName" className="control-label">Name</label>
+            <label htmlFor="inputName" className="control-label">Name</label>
             <input type="text" className="form-control" id="inputName" required name='name' value={props.name}
               onChange={(e)=> props.setName(e.target.value)}/>
         </div>
@@ -24,7 +24,7 @@ const Name = (props)=>{
 const Phone = (props)=>{
     return(
         <div className="form-group">
-            <label for="phone" className="control-label">Phone</label>
+            <label htmlFor="phone" className="control-label">Phone</label>
             <input type="text" className="form-control" id="phone" required name='phone'
               value={props.phone} onChange={(e)=>props.setPhone(e.target.value)}/>
         </div>
@@ -34,12 +34,12 @@ const Phone = (props)=>{
 const Password = (props)=>{
     return(
         <div className="form-group">
-            <label for="inputPassword" className="control-label" name='password'>Password</label>
+            <label htmlFor="inputPassword" className="control-label" name='password'>Password</label>
                 <input type="password" data-minlength="8" className="form-control" id="inputPassword" placeholder="Password" required name="password"
                   value={props.password} onChange={(e)=>props.setPassword(e.target.value)}/>
                 <div className="help-block"></div>
             
-            <label for="inputPassword" className="control-label" name='password'>Confirm Password</label>
+            <label htmlFor="inputPassword" className="control-label" name='password'>Confirm Password</label>
                 <input type="password" className="form-control" id="inputPasswordConfirm" data-match="#inputPassword" data-match-error="don't match" placeholder="Confirm" required
                  value={props.confirmPassword} onChange={(e)=>props.setConfirmPassword(e.target.value)}/>
                 <div className="help-block with-errors"></div>
