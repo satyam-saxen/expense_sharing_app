d18c0e8adffb5eb59e59d89d32b21a740998ff20
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-21 23:01:18 +0530

    Remove initial rendering page and set body for rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './components/signUp/signUp';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <main>
+        <SignUp></SignUp>
+      </main>
+      <footer>
+        <div>Presented By Technogise Interns 2k21</div>
+      </footer>
     </div>
+    
   );
 }
 
