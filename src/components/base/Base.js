372994dbad1748615fdff29fce266118a80c8064
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-21 23:46:33 +0530

    Add base component and style for login signup forms

---
+++ src/components/base/Base.js
@@ -0,0 +1,12 @@
+import './Base.css';
+import Form from '../form/form';
+const Base = ()=>{
+    return(<div className="container">
+        <section id="content" >
+            <Form></Form>
+        </section>
+    </div>);
+}
+
+
+export default Base;
