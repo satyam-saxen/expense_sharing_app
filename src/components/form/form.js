22969a8e471dc2c314ce0e86806b445c139d41e3
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-22 14:02:27 +0530

    Add validator for phone number and Pass the control to API component

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,10 +1,38 @@
 import SignUp from './../signUp/SignUp';
+import {useState} from 'react';
+import API from './../signUp/signUpApi';
 
 const Form = ()=>{
+    let [name,setName]=useState('');
+    let [phone,setPhone]=useState('');
+    let [password,setPassword]=useState('');
+    let [confirmPassword,setConfirmPassword]=useState('');
+    let params = {
+        "name":name,
+        "setName":setName,
+        "phone":phone,
+        "setPhone":setPhone,
+        "password":password,
+        "setPassword":setPassword,
+        "confirmPassword":confirmPassword,
+        "setConfirmPassword":setConfirmPassword
+    };
+
+    const validatePhone = (e)=>{
+        e.preventDefault();
+        if(phone.length !== 10 || password !== confirmPassword){
+            alert("Invalid Phone or password mismatch!");
+            return(<div></div>);
+        }else{
+            return API(params);      
+        }
+        
+    }
+
     return(
         <div>
-            <form data-toggle="validator"  method="POST" action="/register">
-                <SignUp></SignUp>
+            <form data-toggle="validator"  onSubmit={validatePhone}>
+                <SignUp params={params}></SignUp>
             </form>
         </div>
     )
