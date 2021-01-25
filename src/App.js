e439c3ce342df04b5f10044e87ea0eb2e5a4b0af
Satyam Saxena <satyam2saxena@gmail.com>
2021-01-25 16:11:50 +0530

    Bug Fixed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header></header>
-      <main>
-        <Base></Base>
+      <main className="row">
+        <Base className="center"></Base>
       </main>
       <footer>
         <div>Presented By Technogise Interns 2k21</div>
