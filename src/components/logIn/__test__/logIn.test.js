20cecaedddae6ddaf4d5be059f8d3045f60a89a7
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-28 18:48:22 +0530

    Add test for valid phone number

---
+++ src/components/logIn/__test__/logIn.test.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import Form from './../../form/form';
+import { render,cleanup , screen,fireEvent} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
+afterEach(cleanup);
+
+describe("SignUp component",()=>{
+
+    it('check for Phone number to be of 10 digits', () => {
+
+        const PhoneNumber = '1234567891';
+        render(<Form></Form>)
+        expect(screen.getByLabelText('Phone').value).toBe("") // empty before
+        fireEvent.change(screen.getByLabelText('Phone'), { target: { value: PhoneNumber } })
+        expect(screen.getByLabelText('Phone').value).toBe(PhoneNumber) //empty after
+        
+    });
+
+});
