c9e94f3a87ead22569ff4ddea0e6d7833982f664
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-28 16:11:43 +0530

    Add content and form shape for the app

---
+++ src/components/base/Base.js
@@ -0,0 +1,13 @@
+import Form from '../form/form';
+import './../form/form.css';
+
+const Base = ()=>{
+    return(<div className="container">
+        <section id="content" >
+            <Form></Form>
+        </section>
+    </div>);
+}
+
+
+export default Base;
