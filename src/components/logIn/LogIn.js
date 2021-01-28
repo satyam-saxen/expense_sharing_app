1095d92b4f1a14fdc6952d38211709b8fdbc4f17
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-28 18:46:09 +0530

    fixed bug related to phone section

--- src/components/logIn/LogIn.js
+++ src/components/logIn/LogIn.js
@@ -1,14 +1,14 @@
 
-function LogIn(props) {
+const LogIn=(props) => {
     return (
         <div>
             <h1>Expense Sharing App</h1>
-            <h3>Log In</h3>
+            <h2>Log In</h2>
             <Phone phone={props.params.phone} setPhone={props.params.setPhone}></Phone>
             <Password password={props.params.password} setPassword={props.params.setPassword}></Password>
-            <Button></Button>
-            <h5>Do not have an account yet ?</h5>
-            <a href="http://www.technogise.com"> Register Here </a>
+            <Button id="signInButton"></Button>
+            <h5>_______</h5>
+            <a id="register" href="http://www.technogise.com"> Register Here </a>
         </div>
     );
 }
@@ -17,8 +17,8 @@ function LogIn(props) {
 const Phone = (props)=>{
     return(
         <div className="form-group">
-            <label htmlFor="phone" className="control-label">Phone</label>
-            <input type="number" className="form-control" id="phone" required={true} name='phone'
+            <label id="phoneText" htmlFor="phoneBox" className="control-label">Phone</label>
+            <input type="number" className="form-control" id="phoneBox" required={true} name='phone'
                 pattern="\d{10}" placeholder="10 digit Phone Number"
               value={props.phone} onChange={(e)=>props.setPhone(e.target.value)}/>
         </div>
@@ -29,9 +29,10 @@ const Phone = (props)=>{
 const Password = (props)=>{
     return(
         <div className="form-group">
-            <label htmlFor="inputPassword" className="control-label" name='password'>Password</label>
-                <input type="password" data-minLength={8} className="form-control" id="inputPassword" placeholder="Password" required={true} name="password"
+            <label id="pass" htmlFor="inputPassword" className="control-label" name='password'>Password</label>
+                <input type="password" data-minlength={8} className="form-control" id="inputPassword" placeholder="Password" required={true} name="password"
                   value={props.password} onChange={(e)=>props.setPassword(e.target.value)}/>
+                <div className="help-block"></div>
         </div>
     );
 }
@@ -40,7 +41,7 @@ const Password = (props)=>{
 const Button = ()=>{
     return(
         <div className="form-group">
-            <button type="submit" className="btn btn-primary">Sign In</button>
+            <button type="submit" className="btn btn-primary" id="signButton">Sign In</button>
         </div>
     );
 }
