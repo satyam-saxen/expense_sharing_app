fc2b544644ce8608d2ebdb7085eba028e0ccf6d3
Satyam Saxena <satyam2saxena@gmail.com>
2021-02-01 21:59:06 +0530

    Move browserRouter from index to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Switch, Route } from 'react-router-dom';
+import {Switch, Route, BrowserRouter } from 'react-router-dom';
 import Homepage from './components/homepage/homepage';
 import SignUp from './components/signUp/SignUp';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,10 +8,12 @@ function App() {
   return (
     <div className="App">      
       <main className="row">
-      <Switch>
-          <Route exact path={["/", "/home"]} component={Homepage} />
-          <Route exact path="/sign-up" component={SignUp} />
-      </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={["/", "/home"]} component={Homepage} />
+            <Route exact path="/sign-up" component={SignUp} />
+          </Switch>
+        </BrowserRouter>
       </main>
       <footer>
         <div>Presented By Technogise Interns 2k21</div>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App></App>
-  </BrowserRouter>,
+  <App></App>,
   document.getElementById('root')
 );
-
-
