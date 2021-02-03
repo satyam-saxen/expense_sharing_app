1d7ddcd782ea6470fed44edb1594450ffdc85618
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-02-03 12:11:41 +0530

    Remove Tests

--- src/components/logIn/__test__/logIn.test.js
+++ src/components/logIn/__test__/logIn.test.js
@@ -1,51 +1,51 @@
-import React from 'react';
-import LogIn from './../logIn';
-import { render,cleanup , screen,fireEvent} from '@testing-library/react';
-import "@testing-library/jest-dom/extend-expect";
+// import React from 'react';
+// import LogIn from './../logIn';
+// import { render,cleanup , screen,fireEvent} from '@testing-library/react';
+// import "@testing-library/jest-dom/extend-expect";
 
-afterEach(cleanup);
+// afterEach(cleanup);
 
-describe("Form", () => {
-    it("should render the basic fields", () => {
-      render(<LogIn></LogIn>);
-      expect(
-        screen.getByRole("heading", { name: "Log In" })
-      ).toBeInTheDocument();
+// describe("Form", () => {
+//     it("should render the basic fields", () => {
+//       render(<LogIn></LogIn>);
+//       expect(
+//         screen.getByRole("heading", { name: "Log In" })
+//       ).toBeInTheDocument();
   
-      expect(
-          screen.getByRole("spinbutton", { name: /phone/i })
-        ).toBeInTheDocument();
+//       expect(
+//           screen.getByRole("spinbutton", { name: /phone/i })
+//         ).toBeInTheDocument();
   
-      expect(
-        screen.getByPlaceholderText(/Password/i)
-      ).toBeInTheDocument();
+//       expect(
+//         screen.getByPlaceholderText(/Password/i)
+//       ).toBeInTheDocument();
       
-      expect(screen.getByRole("button")).toBeInTheDocument();
+//       expect(screen.getByRole("button")).toBeInTheDocument();
   
-    });
-  });
-  
-  
-
-// describe("SignUp component",()=>{
-
-//     it('should check for the phone to be of digits', () => {
-
-//         const PhoneNumber = '8865011413';
-//         render(<LogIn></LogIn>)
-//         expect(screen.getByLabelText('Phone').value).toBe('') // empty before
-//         fireEvent.change(screen.getByLabelText('Phone'), { target: { value: PhoneNumber } })
-//         expect(screen.getByLabelText('Phone').value).toBe(PhoneNumber) //empty after
-        
 //     });
+//   });
+  
+  
 
-//     it('should not allow letters to be inputted', () => {
-//         const randomString = 'Good Day';
-//         render(<LogIn></LogIn>)
-//         expect(screen.getByLabelText('Phone').value).toBe('') // empty before
-//         fireEvent.change(screen.getByLabelText('Phone'), { target: { value: randomString } })
-//         expect(screen.getByLabelText('Phone').value).toBe('') //empty after
+// // describe("SignUp component",()=>{
+
+// //     it('should check for the phone to be of digits', () => {
+
+// //         const PhoneNumber = '8865011413';
+// //         render(<LogIn></LogIn>)
+// //         expect(screen.getByLabelText('Phone').value).toBe('') // empty before
+// //         fireEvent.change(screen.getByLabelText('Phone'), { target: { value: PhoneNumber } })
+// //         expect(screen.getByLabelText('Phone').value).toBe(PhoneNumber) //empty after
         
-//     });
+// //     });
 
-// });
+// //     it('should not allow letters to be inputted', () => {
+// //         const randomString = 'Good Day';
+// //         render(<LogIn></LogIn>)
+// //         expect(screen.getByLabelText('Phone').value).toBe('') // empty before
+// //         fireEvent.change(screen.getByLabelText('Phone'), { target: { value: randomString } })
+// //         expect(screen.getByLabelText('Phone').value).toBe('') //empty after
+        
+// //     });
+
+// // });
