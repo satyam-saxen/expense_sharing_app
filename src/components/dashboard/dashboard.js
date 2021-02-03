665c94e459c651508c679aba02e77768aa6a6e29
Satyam Saxena <satyam2saxena@gmail.com>
2021-02-03 08:21:24 +0530

    Add debt card and fix the height on card container

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -7,7 +7,7 @@ const Dashboard = ()=>{
                 <div className="dashboard shadow p-3 mb-5 rounded">
                     <div className="row align-items-center nav shadow p-3">
                         <div className="col-md-4">
-                            <h2>Dashboard</h2>
+                            <h4>Dashboard</h4>
                         </div>
                         <div className="offset-md-3 col-md-5 justify-item-end">
                             <button class="btn btn-primary add-expense-btn">Add an Expense</button>
@@ -15,35 +15,73 @@ const Dashboard = ()=>{
                     </div>
                     <div className="row">
                         <div className="col-md-6 p-3 shadow">
-                            <div className="row shadow-lg p-3 mb-5 rounded">
+                            <div className="row shadow-lg p-3 rounded">
                                 <div className="col-md-4">
-                                    <h5>Debt List</h5>
+                                    <h5>Debts</h5>
                                 </div>
                                 <div className="col-md-8">
 
                                 </div>
                             </div>
-                            <div className="row">
-                                <p>yrquwysd sfshs dhfs ifhskdjskfdjksdfjsd sjkdjsk dsjdklaksjdaidjliwjdaijaskdla maskdks a dnsaksd ad 
-                                    dkajkjadk akjdkajsdi sajdaij wi w asjdaldj wli da dsjdiad adjakd l adhfweiwjo f scn irejaifjdkanjfnakalnk 
-                                    ksmkdfksjfkdsjfksdjkdkan akj jaidji wijd a
-                                </p>
+                            <div className="card-container">                                
+                                <div className="row shadow-sm p-3 rounded debt-card">
+                                    <div className="col-md-9 card-item">
+                                        <h6>Satyam Saxena</h6>
+                                        <p>owes you</p>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <p><strong>$75.0</strong></p>
+                                    </div>
+                                </div>
+                                <div className="row shadow-sm p-3 rounded debt-card">
+                                    <div className="col-md-9 card-item">
+                                        <h6>Satyam Saxena</h6>
+                                        <p>owes you</p>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <p><strong>$75.0</strong></p>
+                                    </div>
+                                </div>
+                                <div className="row shadow-sm p-3 rounded debt-card">
+                                    <div className="col-md-9 card-item">
+                                        <h6>Satyam Saxena</h6>
+                                        <p>owes you</p>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <p><strong>$75.0</strong></p>
+                                    </div>
+                                </div>
+                                <div className="row shadow-sm p-3 rounded debt-card">
+                                    <div className="col-md-9 card-item">
+                                        <h6>Satyam Saxena</h6>
+                                        <p>owes you</p>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <p><strong>$75.0</strong></p>
+                                    </div>
+                                </div>
+                                <div className="row shadow-sm p-3 rounded debt-card">
+                                    <div className="col-md-9 card-item">
+                                        <h6>Satyam Saxena</h6>
+                                        <p>owes you</p>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <p><strong>$75.0</strong></p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="col-md-6 p-3 shadow">
-                            <div className="row shadow-lg p-3 mb-5 rounded">
+                            <div className="row shadow-lg p-3 rounded">
                                 <div className="col-md-5">
-                                    <h5>Expense List</h5>
+                                    <h5>Expenses</h5>
                                 </div>
                                 <div className="col-md-7">
 
                                 </div>
                             </div>
                             <div className="row">
-                                <p>yrquwysd sfshs dhfs ifhskdjskfdjksdfjsd sjkdjsk dsjdklaksjdaidjliwjdaijaskdla maskdks a dnsaksd ad 
-                                    dkajkjadk akjdkajsdi sajdaij wi w asjdaldj wli da dsjdiad adjakd l adhfweiwjo f scn irejaifjdkanjfnakalnk 
-                                    ksmkdfksjfkdsjfksdjkdkan akj jaidji wijd a
-                                </p>
+                                
                             </div>
                         </div>
                     </div>
