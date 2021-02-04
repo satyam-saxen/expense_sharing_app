3987498210ee5c1bb160fd62ee2da5380bf64c7a
Satyam Saxena <satyam2saxena@gmail.com>
2021-02-04 15:40:58 +0530

    Add responsive faeture for small devices

--- src/components/cards/debt/debtCard.js
+++ src/components/cards/debt/debtCard.js
@@ -3,11 +3,11 @@ import './debtCard.css';
 const DebtCard = ({debt})=>{
     return(
         <div className="row shadow-sm p-3 rounded debt-card">
-            <div className="col-md-8 card-item">
+            <div className="col-md-8 col-sm-8 col-8 card-item">
                 <h6>{debt.debtor !== null ? debt.debtor : debt.creditor}</h6>
                 <>{debt.debtor !== null ? <p className="debtor-desc">owes you</p> : <p className="creditor-desc">you owe</p>}</>
             </div>
-            <div className="col-md-4">
+            <div className="col-md-4 col-sm-4 col-4">
                 <p><strong>{debt.amount}Rs.</strong></p>
             </div>
         </div> 
--- src/components/cards/expense/expenseCard.js
+++ src/components/cards/expense/expenseCard.js
@@ -5,7 +5,7 @@ const month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov'
 const ExpenseCard = ({expense})=>{
     return(
         <div className="row shadow-sm p-2 rounded expense-card">
-            <div className="col-md-3">
+            <div className="col-md-3 col-sm-3 col-3">
                 <figure>
                     <header>
                         {month[expense.dateTime.getMonth()]}
@@ -15,15 +15,15 @@ const ExpenseCard = ({expense})=>{
                     </section>
                 </figure>
             </div>  
-            <div className="col-md-8">
+            <div className="col-md-8 col-sm-8 col-8">
                 <div className="row">
                     <h5>{expense.description}</h5>
                 </div>
                 <div className="row">
-                    <div className="col-md-9 expense-desc">
+                    <div className="col-md-9 col-sm-9 col-9 expense-desc">
                         <p>Paid by-{expense.payerName}</p>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-3 col-sm-3 col-3">
                         <p>Rs{expense.amount}</p>
                     </div>
                 </div>
