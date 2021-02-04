22d09cd5014787ac03f0c150e4263140f876ab64
Satyam Saxena <satyam2saxena@gmail.com>
2021-02-04 14:08:19 +0530

    Add tests for cards components

---
+++ src/components/cards/__test__/debtCard.test.js
@@ -0,0 +1,41 @@
+import "@testing-library/jest-dom/extend-expect";
+import { cleanup, render} from '@testing-library/react';
+import DebtCard from "../debt/debtCard";
+
+afterEach(cleanup);
+
+describe("DebtCard",()=>{
+    it("renders the debt card with data",async ()=>{
+        const debt={
+            debtor:"A",
+            creditor:"B",
+            amount:"20"
+        };
+        const {findByText} = await render(<DebtCard debt={debt}></DebtCard>);
+        expect(await findByText('owes you')).toBeInTheDocument();
+        expect(await findByText('A')).toBeInTheDocument();
+        expect(await findByText('20Rs.')).toBeInTheDocument();
+    });
+
+    it("renders the debt card when the debtor is null",async ()=>{
+        const debt={
+            debtor:null,
+            creditor:"B",
+            amount:"20"
+        };
+        const {findByText} = await render(<DebtCard debt={debt}></DebtCard>);
+        expect(await findByText('you owe')).toBeInTheDocument();
+    });
+
+    it("renders the debt card when the creditor is null",async ()=>{
+        const debt={
+            debtor:"A",
+            creditor:null,
+            amount:"20"
+        };
+        const {findByText} = await render(<DebtCard debt={debt}></DebtCard>);
+        expect(await findByText('owes you')).toBeInTheDocument();
+    });
+
+    
+});
---
+++ src/components/cards/__test__/expenseCard.test.js
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom/extend-expect";
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import ExpenseCard from './../expense/expenseCard';
+
+afterEach(cleanup);
+
+describe("ExpenseCard",()=>{
+    it("renders the expense card with data",async ()=>{
+        const expense={
+            description:"Holiday",
+            amount:"200",
+            payerName:"Prans",
+            dateTime:new Date()
+        };
+        const {findByText} = await render(<ExpenseCard expense={expense}></ExpenseCard>);
+        expect(await findByText('Holiday')).toBeInTheDocument();
+        expect(await findByText('Paid by-Prans')).toBeInTheDocument();
+        expect(await findByText('Rs200')).toBeInTheDocument();
+        expect(await findByText('Feb')).toBeInTheDocument();
+        expect(await findByText('4')).toBeInTheDocument();
+    });
+
+});
