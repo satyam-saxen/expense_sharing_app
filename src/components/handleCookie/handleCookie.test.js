2547dfd07fe1484a3346821a6bd32f01767ca6dd
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-02-04 11:00:18 +0530

    Add test for authorization token

---
+++ src/components/handleCookie/handleCookie.test.js
@@ -0,0 +1,26 @@
+
+import handleCookie from "./handleCookie";
+
+import Cookies from 'universal-cookie';
+const Cookie = new Cookies();
+
+it('checks the storage of cookie after setting one', () => {
+
+    handleCookie.setCookie('esaUserToken', "User Looged In");
+    expect(handleCookie.getCookie('esaUserToken')).toBe("User Looged In");
+  
+})
+
+  it('checks whether a cookie is deleted successfully after delete command', () => {
+
+    handleCookie.setCookie('esaUserToken', "User Looged In");
+    handleCookie.deleteCookie('esaUserToken');
+    expect(handleCookie.getCookie('esaUserToken')).toBe(undefined);
+
+})
+
+  it('checks if a cookie is present without setting one', () => {
+
+    expect(handleCookie.getCookie('esaUserToken')).toBe(undefined);
+
+})
