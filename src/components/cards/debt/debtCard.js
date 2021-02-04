774963f8d65bff11386f60955e58643e13131c4b
Satyam Saxena <satyam2saxena@gmail.com>
2021-02-04 14:08:57 +0530

    Remove nested paragraphs

--- src/components/cards/debt/debtCard.js
+++ src/components/cards/debt/debtCard.js
@@ -5,7 +5,7 @@ const DebtCard = ({debt})=>{
         <div className="row shadow-sm p-3 rounded debt-card">
             <div className="col-md-8 card-item">
                 <h6>{debt.debtor !== null ? debt.debtor : debt.creditor}</h6>
-                <p>{debt.debtor !== null ? <p className="debtor-desc">owes you</p> : <p className="creditor-desc">you owe</p>}</p>
+                <>{debt.debtor !== null ? <p className="debtor-desc">owes you</p> : <p className="creditor-desc">you owe</p>}</>
             </div>
             <div className="col-md-4">
                 <p><strong>{debt.amount}Rs.</strong></p>
