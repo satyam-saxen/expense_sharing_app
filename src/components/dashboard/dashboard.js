2c30e662af96dd6da297835c005e5998c40b58b6
Satyam Saxena <satyam2saxena@gmail.com>
2021-02-05 12:05:25 +0530

    Add dashboard user's name and response as per the response from API dynamically

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,60 +3,36 @@ import ExpenseCard from '../cards/expense/expenseCard';
 import handleCookie from './../handleCookie/handleCookie';
 import startUpData from './../../services/dataService/dataService';
 import Homepage from './../homepage/homepage';
+import {useState, useEffect} from 'react';
 import './dashboard.css';
 
 
 const Dashboard = (props)=>{
-    // let response = null;
-    let response = {
-        expenses:[
-            {
-                description:"Valentine Dinner",
-                amount:"2000",
-                payerName:"Aaryam Saxena",
-                dateTime:new Date()
-            }
-        ],
-        debts:[
-            {
-                debtor:"Satyam Saxena",
-                creditor:null,
-                amount:"80"
-            },
-            {
-                debtor:null,
-                creditor:"Shubh",
-                amount:"60"
-            },
-        ],
-        user:"Satyam"
-    };
-    async function loadingData(token) {
-        try {
-            response = await startUpData({token});
-            if(response.status === 200) {
-                response = await response.json();
-            }else if(response.status === 401) {
-                alert("Session has been expired, login again!");
+    let [resp,setResp] = useState(null);
+    
+    useEffect(()=>{
+        async function loadingData(token) {
+            try {
+                let response = await startUpData({token});
+                if(response.status === 200) {
+                    setResp(await response.json());
+
+                }else if(response.status === 401) {
+                    alert("Session has been expired, login again!");
+                    props.history.push('/login');
+                }
+            }catch(e) {
+                console.log(e.message);
+                alert("Unexpected error occured please login again");
                 props.history.push('/login');
             }
-        }catch(e) {
-            console.log(e.message);
-            alert("Unexpected error occured please login again");
-            props.history.push('/login');
         }
-    }
+    
+        if(handleCookie.getCookie('esaUserToken')) {
+            loadingData(handleCookie.getCookie('esaUserToken')); 
+        }
+    },[]);
 
-    if(handleCookie.getCookie('esaUserToken')) {
-        // loadingData(handleCookie.getCookie('esaUserToken')); //Uncomment this line while running server
-    }
-
-    if(response === null) {
-        response = {
-            expenses:[],
-            debts:[]
-        };
-    }
 
     return(
         <>
@@ -66,7 +42,7 @@ const Dashboard = (props)=>{
                 <div className="dashboard shadow p-3 mb-5 rounded">
                     <div className="row align-items-center nav shadow p-3">
                         <div className="col-md-4">
-                            <h5>{response.user}'s Dashboard</h5>
+                            <h5>{resp!==null ? resp.user.name : "Unknown"}'s Dashboard</h5>
                         </div>
                         <div className="offset-md-3 col-md-5 justify-item-end">
                             <button className="btn btn-primary add-expense-btn">Add an Expense</button>
@@ -83,9 +59,9 @@ const Dashboard = (props)=>{
                                 </div>
                             </div>
                             <div className="card-container">                                
-                                {response.debts.map((debt)=>(
+                                {resp!==null ? resp.debts.map((debt)=>(
                                     <DebtCard debt={debt}></DebtCard>
-                                ))}                            
+                                )) : null}                            
                             </div>
                         </div>
                         <div className="col-md-6 p-3 shadow">
@@ -98,9 +74,9 @@ const Dashboard = (props)=>{
                                 </div>
                             </div>
                             <div className="card-container">  
-                                {response.expenses.map((expense)=>(
+                                {resp!==null ? resp.expenses.map((expense)=>(
                                         <ExpenseCard expense={expense}></ExpenseCard>
-                                ))}                              
+                                )) : null}                              
                                 
                             </div>
                         </div>
